d12e7476d0d256a33a55b0256c15b9f01d0992c0
Jason Alfrey <jason.alfreyy@gmail.com>
2026-07-27 20:08:39 -0500

    Create custom user overrides file

---
+++ user-overrides.js
@@ -0,0 +1,9 @@
+user_pref("browser.formfill.enable", true);
+user_pref("browser.cache.disk.enable", true);
+user_pref("dom.security.https_only_mode.upgrade_local", true);
+user_pref("browser.download.useDownloadDir", true);
+user_pref("privacy.sanitize.sanitizeOnShutdown", false);
+user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", false);
+user_pref("privacy.clearSiteData.cache", false); // [DEFAULT: true]
+user_pref("privacy.clearSiteData.formdata", false);
+user_pref("privacy.clearHistory.cache", false); // [DEFAULT: true]
